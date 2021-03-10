8567dd61795d040308ab90ea3b39a4a84ecddf4f
Chris Padilla <cdpadilla42@gmail.com>
2021-03-10 10:40:41 -0600

    Add form logic

--- imports/ui/GuestForm.js
+++ imports/ui/GuestForm.js
@@ -6,12 +6,47 @@ const GuestForm = () => {
     message: '',
   });
 
+  function handleChange(e) {
+    setInputs({
+      ...inputs,
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  }
+
+  function clearForm() {
+    setInputs({
+      name: '',
+      message: '',
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(inputs);
+    clearForm();
+  }
+
   return (
-    <form className="post_form" method="POST">
+    <form className="post_form" method="POST" onSubmit={handleSubmit}>
       <fieldset>
         <h2>Leave a message!</h2>
-        <input type="text" name="name" id="name" placeholder="Your name!" />
-        <textarea name="message" id="message" cols="30" rows="10"></textarea>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Your name!"
+          value={inputs.name}
+          onChange={handleChange}
+        />
+        <textarea
+          name="message"
+          id="message"
+          cols="30"
+          rows="10"
+          value={inputs.message}
+          onChange={handleChange}
+        ></textarea>
+        <input type="submit" value="Post" />
       </fieldset>
     </form>
   );
