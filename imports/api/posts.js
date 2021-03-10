4f2f3cff4a8a720c2d607b6850f90f2e28e7c0d5
Chris Padilla <cdpadilla42@gmail.com>
2021-03-10 09:27:48 -0600

    Write schema for posts

--- imports/api/posts.js
+++ imports/api/posts.js
@@ -1,3 +1,9 @@
 import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+PostsCollection.schema = new SimpleSchema({
+  name: { type: String },
+  message: { type: String },
+});
 
 export const PostsCollection = new Mongo.Collection('posts');
